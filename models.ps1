7664fdf3d2619c097772563017e4c622da480ce6
calemccammon <calemccammon@gmail.com>
2021-02-07 21:55:55 -0500

    definitions, monsters

--- models.ps1
+++ models.ps1
@@ -1,11 +1,11 @@
-New-Item -Force -Path .\includes.oajs
+New-Item -Force -Path .\models.oajs
 Get-ChildItem scripts -Recurse *.oajs | ForEach {
 	$path = $($_.Directory.Name)
-	if ($path -ne "definitions") {
+	if ($path -eq "models") {
 		if ($path -ne "scripts") {
 			$path = "scripts/$($_.Directory.Name)"
 		}
 		$variable = "//#include $path/$($_.Name)"
-		Add-Content -Value $variable -Path .\includes.oajs
+		Add-Content -Value $variable -Path .\models.oajs
 	}
 }
